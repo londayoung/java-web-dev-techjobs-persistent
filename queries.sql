3fa2fbd324204124cc68932fb2100c94e90f9e8b
Londa Young <londayoung@LondaKNOWiNKmacbookPro.local>
2020-02-01 17:56:48 -0600

    assignment 4

--- queries.sql
+++ queries.sql
@@ -1,7 +1,23 @@
 ## Part 1: Test it with SQL
+SELECT *
+FROM jobs
 
 ## Part 2: Test it with SQL
 
+SELECT name
+FROM employer
+WHERE location = "St. Louis City"
+
 ## Part 3: Test it with SQL
 
+DROP TABLE jobs;
+
+
 ## Part 4: Test it with SQL
+
+SELECT name, description
+FROM skill
+JOIN job_skills
+ON skill.id = job_skills.skills_id
+WHERE job_skills.jobs_id is not null
+ORDER BY name asc
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,32 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotBlank;
+import java.util.ArrayList;
+import java.util.List;
+//
+@Entity
 public class Employer extends AbstractEntity {
 
+
+    @OneToMany
+    @JoinColumn
+    private final List<Job> jobs = new ArrayList<>();
+
+
+    @NotBlank(message=  "Job location is required")
+    private String location;
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Employer(){};
 
 }
